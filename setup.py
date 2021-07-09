a842052e9c3d9ef776c2795cd50600a21539cc20
rlaphoenix <rlaphoenix@pm.me>
2021-07-09 03:24:54 +0100

    setup: Heavily improve classifiers

--- setup.py
+++ setup.py
@@ -12,13 +12,22 @@ setup(
     license="MIT",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/rlaPHOENiX/pympls",
+    url="https://github.com/rlaphoenix/pympls",
     packages=find_packages(),
     install_requires=[],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Multimedia :: Video"
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.6"
 )
